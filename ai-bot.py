0a88dcf44d7026963601798d02fda06a9d2ec4cd
Jaynil Patel <jaynilbvb@gmail.com>
2018-06-24 12:39:18 +0530

    build assimilator and geyser

--- ai-bot.py
+++ ai-bot.py
@@ -1,7 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 class OurCustomBot(sc2.BotAI):
     async def on_step(self, iteration):
@@ -9,6 +9,8 @@ class OurCustomBot(sc2.BotAI):
         await self.distribute_workers()
         await self.build_workers()
         await self.build_pylons()
+        await self.expand()
+        await self.build_assimilator()
 
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.noqueue:
@@ -21,6 +23,22 @@ class OurCustomBot(sc2.BotAI):
             if nexuses.exists:
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near= nexuses.first)
+
+    async def expand(self):
+        if self.units(NEXUS).amount < 2 and self.can_afford(NEXUS):
+            await self.expand_now()
+
+    async def build_assimilator(self):
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vespene_geyser.closer_than(15, nexus)
+            for vaspene in vaspenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vaspene.position)
+                if worker is None:
+                    break
+                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
             
 run_game(maps.get("AbyssalReefLE"),[
     Bot( Race.Protoss, OurCustomBot()),
