43dc2e590d11f7a3fc14ac0ca2b82ee79e7c27fd
Jaynil Patel <jaynilbvb@gmail.com>
2018-06-27 03:18:11 +0530

    defining strategy for attacking

--- ai-bot.py
+++ ai-bot.py
@@ -3,6 +3,7 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, \
 GATEWAY, STALKER, CYBERNETICSCORE
+import random
 
 class OurCustomBot(sc2.BotAI):
     async def on_step(self, iteration):
@@ -14,6 +15,7 @@ class OurCustomBot(sc2.BotAI):
         await self.build_assimilator()
         await self.build_offensive_force_buildings()
         await self.build_offensive_force()
+        await self.attack()
         
 
     async def build_workers(self):
@@ -59,6 +61,28 @@ class OurCustomBot(sc2.BotAI):
         for gw in self.units(GATEWAY).ready.noqueue:
             if self.can_afford(STALKER) and self.supply_left > 0:
                 await self.do(gw.train(STALKER))
+
+    def find_target(self, state):
+        if len(self.known_enemy_units) > 0:
+            return random.choice(self.known_enemy_units) # choose random enemy-units once we reach the enemy_start_location 
+        elif len(self.known_enemy_structures) > 0:       #choose random enemy-structures once we reach the enemy_start_location
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]  #if no activity is currently performed then go to enemy_start_location
+    ''' 
+        To attack an enemy, there are 2 ways:
+        1) We build our army (>15 units) and find the enemy start(base) location using find_traget() and attack.
+        2) If enemy attacks us, and if we have >3 STALKERS, then attack that enemy
+    '''
+    async def attack(self):
+        if self.units(STALKER).amount > 15:
+            for s in self.units(STALKER).idle: # for all the idle stalker
+                await self.do(s.attack(self.find_target(self.state))) #attack the based on the return value of find_target. ie enemy-location, enemy-structures, enemy-units
+               
+        elif self.units(STALKER).amount > 3:
+            if len(self.known_enemy_units) > 0:
+                for s in self.units(STALKER).idle:
+                    await self.do(s.attack(random.choice(self.known_enemy_units))) # randomly choose the enemy-unit and then attack
 
 
 run_game(maps.get("AbyssalReefLE"),[
