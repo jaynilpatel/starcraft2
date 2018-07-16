d01208b4ba1c76539bfadc751f800cc41d346183
Jaynil Patel <jaynilbvb@gmail.com>
2018-07-17 01:00:24 +0530

    trained the model

--- build_model.py
+++ build_model.py
@@ -1,6 +1,6 @@
 import keras 
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPool2D
+from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D
 from keras.callbacks import TensorBoard
 import numpy as np 
 import os
@@ -36,3 +36,102 @@ model.compile(loss='categorical_crossentropy', optimizer=opt,
                 metrics = ['accuracy'])
 
 tensorboard = TensorBoard(log_dir = "logs/stage1")
+
+train_data_dir = "train_data"
+
+def check_data():
+    choices = {"no_attacks": no_attacks,
+               "attack_closest_to_nexus": attack_closest_to_nexus,
+               "attack_enemy_structures": attack_enemy_structures,
+               "attack_enemy_start": attack_enemy_start}
+
+    total_data = 0
+
+    lengths = []
+    for choice in choices:
+        print("Length of {} is: {}".format(choice, len(choices[choice])))
+        total_data += len(choices[choice])
+        lengths.append(len(choices[choice]))
+
+    print("Total data length now is:", total_data)
+    return lengths
+
+hm_epochs = 1
+
+for i in range(hm_epochs):
+    current = 0
+    increment = 200
+    not_maximum = True
+    all_files = os.listdir(train_data_dir)
+    maximum = len(all_files)
+    random.shuffle(all_files)
+
+    while not_maximum:
+        print("Working on {}:{}".format(current, current+increment))
+        no_attacks = []
+        attack_closest_to_nexus = []
+        attack_enemy_structures = []
+        attack_enemy_start = []
+
+        for file in all_files[current:current+increment]:
+            full_path = os.path.join(train_data_dir, file)
+            data = np.load(full_path)
+            for d in data:
+                choice = np.argmax(d[0])
+                if choice == 0:
+                    no_attacks.append(d)
+                elif choice == 1:
+                    attack_closest_to_nexus.append(d)
+                elif choice == 2:
+                    attack_enemy_structures.append(d)
+                elif choice == 3:
+                    attack_enemy_start.append(d)
+
+        lengths = check_data()
+        lowest_data = min(lengths)
+
+        '''
+        attack_choice = {"no_attacks": no_attacks,
+               "attack_closest_to_nexus": attack_closest_to_nexus,
+               "attack_enemy_structures": attack_enemy_structures,
+               "attack_enemy_start": attack_enemy_start}
+        train_data = []
+
+        for attack in attack_choice:
+            random.shuffle(attack_choice[attack])
+            attack_choice[attack] = attack_choice[attack][:lowest_data]
+            train_data = train_data + attack_choice[attack]
+        ''' 
+        random.shuffle(no_attacks)
+        random.shuffle(attack_closest_to_nexus)
+        random.shuffle(attack_enemy_structures)
+        random.shuffle(attack_enemy_start)
+
+        no_attacks = no_attacks[:lowest_data]
+        attack_closest_to_nexus = attack_closest_to_nexus[:lowest_data]
+        attack_enemy_structures = attack_enemy_structures[:lowest_data]
+        attack_enemy_start = attack_enemy_start[:lowest_data]
+        
+        check_data()
+
+        
+        random.shuffle(train_data)
+        print(len(train_data))
+
+        test_size = 100
+        batch_size = 128
+
+        x_train = np.array([i[1] for i in train_data[:-test_size]]).reshape(-1, 176, 200, 3)
+        y_train = np.array([i[0] for i in train_data[:-test_size]])
+
+        x_test = np.array([i[1] for i in train_data[-test_size:]]).reshape(-1, 176, 200, 3)
+        y_test = np.array([i[0] for i in train_data[-test_size:]])
+
+        model.fit(x_train, y_train, batch_size=batch_size, validation_data=(x_test, y_test), shuffle=True,
+                  verbose=1, callbacks=[tensorboard])
+
+        model.save("BasicCNN-{}-epochs-{}-LR-STAGE1".format(hm_epochs, learning_rate))
+        current += increment
+        if current > maximum:
+            not_maximum = False
+        
